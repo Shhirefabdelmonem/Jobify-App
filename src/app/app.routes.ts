77e32c1125d47ee8dcca57a452607bac50327b5f
me <shirefelageiry@gmail.com>
2025-04-29 16:21:33 +0300

    add home component with routing, header, and hero section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RecommendationsComponent } from './components/recommendations/recommend
 import { ProfileComponent } from './components/profile/profile.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: 'recommendations',component:RecommendationsComponent},
     {path:'profile',component:ProfileComponent},
     {path:'sign-up',component:SignUpComponent},
     {path:'sign-in',component:SignInComponent},
-    {path:'',redirectTo:'/sign-up',pathMatch:'full'}
+    {path:'home',component:HomeComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
